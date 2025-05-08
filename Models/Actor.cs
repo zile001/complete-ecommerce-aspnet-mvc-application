c2310e8d9f7965f386fa16968bdcc0b00d0420bd
zile001 <zivanovicaleksandar15@gmail.com>
2025-05-08 14:07:37 +0200

    Adding model relationship and dbContext

--- Models/Actor.cs
+++ Models/Actor.cs
@@ -9,6 +9,9 @@ namespace eTickets.Models
         public string ProfilePictureURL { get; set; }
         public string FullName { get; set; }
         public string Bio { get; set; }
+
+        //Relationships
+        public List<Actor_Movie> Actors_Movies { get; set; }
     }
     
 }
